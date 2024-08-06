0776a32f1f52f7fa602c15afd079a6b2c4a2c79f
Tony <lous02201234@gmail.lcm>
2024-08-06 15:14:43 +0800

    聖安測試react

--- client/src/app.js
+++ client/src/app.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
- 
-import MemberIndex from './pages/MemberIndex';
-import MemberEdit from './pages/MemberEdit';
-import MemberOderList from './pages/MemberOderList';
-import MemberOder from './pages/MemberOder';
-import MemberTickets from './pages/MemberTickets';
-import MemberCollection from './pages/MemberCollection';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
- 
+import MemberIndex from './pages/MemberIndex'
+import MemberEdit from './pages/MemberEdit'
+import MemberOderList from './pages/MemberOderList'
+import MemberOder from './pages/MemberOder'
+import MemberTickets from './pages/MemberTickets'
+import MemberCollection from './pages/MemberCollection'
+import ProductAll from './pages/ProductAll'
+
 class App extends Component {
   render() {
-    return (      
-       <BrowserRouter>
+    return (
+      <BrowserRouter>
         <div>
-            <Switch>
-             <Route path="/Member/Index" component={MemberIndex} exact/>
-             <Route path="/Member/Edit" component={MemberEdit} />
-             <Route path="/Member/OderList" component={MemberOderList} />
-             <Route path="/Member/Oder" component={MemberOder} />
-             <Route path="/Member/Ticket" component={MemberTickets} />
-             <Route path="/Member/Collection" component={MemberCollection} />
-             
-           </Switch>
-        </div> 
+          <Switch>
+            <Route path="/Member/Index" component={MemberIndex} exact />
+            <Route path="/Member/Edit" component={MemberEdit} />
+            <Route path="/Member/OderList" component={MemberOderList} />
+            <Route path="/Member/Oder" component={MemberOder} />
+            <Route path="/Member/Ticket" component={MemberTickets} />
+            <Route path="/Member/Collection" component={MemberCollection} />
+            <Route path="/Product/All" component={ProductAll} />
+          </Switch>
+        </div>
       </BrowserRouter>
-    );
+    )
   }
 }
- 
-export default App;
 
+export default App
