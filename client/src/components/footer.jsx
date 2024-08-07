10c59f9d92776d16250a90b0f41f9c38e0dc425a
zenin555 <lei.z0329@gmail.com>
2024-08-07 09:14:56 +0800

    辰潔Footer圖片路徑調整

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../assets/basic.css'
+import footerLogo from '../assets/images/riverflow_logo_f.png'
 
 export default function Footer() {
     return(
         <footer>
         <div class="flex container">
-            <img class="logo" src="../assets/images/riverflow_logo_f.png" alt="logo"/>
+            <img class="logo" src={footerLogo} alt="logo"/>
             <div class="flex footer-right">
                 <div class="flex linkList">
                     <a href="#" class="footerLink">關於我們</a>
