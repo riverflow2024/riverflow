e19fe9f2cf2609e6a420dda99c7afe2783b9d509
YuFuSu <purpleperilla99@gmail.com>
2024-08-09 08:27:53 +0800

    綠界v1-2

--- server/app.js
+++ server/app.js
@@ -9,12 +9,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cookieParser())
 app.use(bodyParser.json())
-<<<<<<< HEAD
-app.use(bodyParser.urlencoded({ extended: true }));
-=======
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
->>>>>>> 72e4107251f012ca72d369be59cf93b95e97e405
 
 // Routers
 const userRoutes = require('./routes/users')
@@ -40,11 +36,7 @@ app.use('/riverflow/test',function(req, res, next) {
 
 // app.use('/riverflow/cart', cartRoutes)
 
-<<<<<<< HEAD
-app.use('/riverflow/events/Tobuy', eventTobuyRoutes)
-=======
 app.use('/riverflow/events/Tobuy', authenticateToken, eventTobuyRoutes)
->>>>>>> 72e4107251f012ca72d369be59cf93b95e97e405
 // app.use('/riverflow/orders', orderRoutes)
 
 module.exports = app
